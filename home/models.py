d811e30d97098d65e7f6f9d07964cf4ac319fecc
adarsh <adarshmaurya9415@gmail.com>
2023-11-05 03:55:23 +0530

    added fields to save user data in database for individual phenotype(prakruti)

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
+
+class PrakrutiResult(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Assuming you have a User model
+    vata_score = models.IntegerField()
+    pitta_score = models.IntegerField()
+    kapha_score = models.IntegerField()
+    prakruti_type = models.CharField(max_length=50)
+
+
